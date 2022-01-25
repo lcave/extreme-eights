8ac6793b1422df879323541f263e56de35cd7016
Luca Cave <lucacave2@gmail.com>
2022-01-25 23:24:15 +1100

    fix incorrect id param

--- app/models/lobbies/factory.rb
+++ app/models/lobbies/factory.rb
@@ -5,15 +5,16 @@ module Lobbies
     def self.create!(creator)
       valid_id = false
       friendly_id = nil
+
       until valid_id
         friendly_id = Spicy::Proton.format("%a-%a-%n")
-        valid_id = true unless Lobby.find_by(id: friendly_id)
+        valid_id = true unless Lobby.find_by(friendly_id: friendly_id)
       end
 
       ActiveRecord::Base.transaction do
         lobby = Lobby.create!(
           lobby_leader_id: creator.id,
-          id: friendly_id,
+          friendly_id: friendly_id,
         )
 
         creator.update!(lobby_id: lobby.id)
